2daea91f91221c0de6319575d3a21f46e74c06ae
Philip J. Grandinetti <43141014+pjgrandinetti@users.noreply.github.com>
2025-05-08 09:39:12 -0700

    aliasing of the import library

--- spinOps/c_code/spin.c
+++ spinOps/c_code/spin.c
@@ -804,3 +804,17 @@ void get_hamiltonian_(double complex *operator,
     memset(operator, 0, nstates * nstates * sizeof(double complex)); 
 
 }
+
+void add_zeeman_hamiltonian_(double complex *operator,site_struct *sites)
+{
+    int total_spin_count = sites->number_of_sites;
+    int i_times_2[total_spin_count];
+    for(int i = 0; i < total_spin_count; i++)
+    {
+        i_times_2[i] = (int) sites->spin[i] * 2;
+    }
+    int nstates = number_of_states_(total_spin_count, i_times_2);
+
+    memset(operator, 0, nstates * nstates * sizeof(double complex)); 
+
+}
